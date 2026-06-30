a87cc835ad889ce08829a0ca009629704a71da2d
Genilson Junior <genilsonjuniordossantos33@gmail.com>
2026-06-30 15:51:26 -0300

    feat: Adicionado o método de salvar dados em um arquivo csv

--- src/repository/Repositorio.java
+++ src/repository/Repositorio.java
@@ -3,9 +3,11 @@ package repository;
 import java.util.List;
 
 public interface Repositorio <T>{
-    void salvar(T item);
+    boolean salvar(T item);
 
     List<T> buscarTodos();
 
     T buscarPorId(int id);
+
+    void adicionarArquivo(T item);
 }
